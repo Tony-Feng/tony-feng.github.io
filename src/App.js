25d988a663d9c7a03d32839f97b67c2aabcdc171
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-09 17:05:33 -0400

    app to func

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { lazy } from '@loadable/component';
 import Loading from './pages/loading';
@@ -6,17 +6,15 @@ import Loading from './pages/loading';
 const Home = lazy(() => import('./pages/home'));
 const Error = lazy(() => import('./pages/error'));
 
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Suspense fallback={ <Loading /> }>
-          <Routes>
-            <Route path="/" exact component={ () => <Home /> } />
-            <Route component={ <Error /> } />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    );
-  };
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={ <Loading /> }>
+        <Routes>
+          <Route path="/" exact component={ () => <Home /> } />
+          <Route component={ <Error /> } />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
