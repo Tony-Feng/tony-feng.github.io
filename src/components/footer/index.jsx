5373e1755ba03a841697617b519217fac497d53d
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-09 22:53:34 -0400

    remove last updated entry

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -25,10 +25,7 @@ export default function Footer(props) {
       <AppBar position="static" sx={{ bgcolor: "#9CF", boxShadow: 0, textOverflow: "ellipsis", overflow: "hidden", py: 2 }} ref={ appBarRef }>
         <Grid container spacing={ 0.1 } direction="column" justifyContent="space-evenly" alignItems="center" wrap="nowrap">
           <Grid item zeroMinWidth xs={ 12 }>
-            <Typography noWrap variant="body1" component="div">{ "\u00A9" } YF 2021 - { currentYear } { "\u2022" } Created By: React</Typography> {/* todo: adjust the copyright content */}
-          </Grid>
-          <Grid item zeroMinWidth xs={ 12 }>
-            <Typography noWrap variant="body1" component="div">Last Updated: {}</Typography> {/* todo: add the last update time dynamically, maybe read from file */}
+            <Typography noWrap variant="body1" component="div">{ "\u00A9" } TF 2021 - { currentYear } { "\u2022" } Created By React</Typography> {/* todo: adjust the copyright content */}
           </Grid>
         </Grid>
       </AppBar>
