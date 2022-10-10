5471be0eaf4ef9a65cb1264c50670c2b3f45d1b6
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-10 12:52:37 -0400

    revert loading page setup

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { store, persistor } from './store';
-import Loading from './pages/loading';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={ store }>
-      <PersistGate loading={ <Loading /> } persistor={ persistor }>
+      <PersistGate loading={ null } persistor={ persistor }>
         <App />
       </PersistGate>
     </Provider>
