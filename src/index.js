fd3fc25cb322911175158872481c5fe120d773f1
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-10 12:51:34 -0400

    try to use loading page in persist gate

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { store, persistor } from './store';
+import Loading from './pages/loading';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={ store }>
-      <PersistGate loading={ null } persistor={ persistor }>
+      <PersistGate loading={ <Loading /> } persistor={ persistor }>
         <App />
       </PersistGate>
     </Provider>
