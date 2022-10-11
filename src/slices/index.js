62273bc7eb135688eb553989ba28be79460e3081
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-10 22:23:15 -0400

    try to modify to use storage and storage session

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,12 +1,34 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import storageSession from 'redux-persist/lib/storage/session';
+import { persistReducer } from 'redux-persist';
 import isDarkReducer from './is-dark-slice';
 import isFirstTimeReducer from './is-first-time-slice';
 import userInfoReducer from './user-info-slice';
 
-export default combineReducers(
+const rootPersistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["userInfo"]
+};
+
+const userInfoPersistConfig = {
+  key: "userInfo",
+  storage: storageSession
+};
+
+const rootReducer = combineReducers(
   {
-    isDarkRdc: isDarkReducer,
-    isFirstTimeRdc: isFirstTimeReducer,
-    userInfoRdc: userInfoReducer
+    userInfoRdc: persistReducer(userInfoPersistConfig, userInfoReducer),
+    other: otherReducer
   }
 );
+
+
+// export default combineReducers(
+//   {
+//     isDarkRdc: isDarkReducer,
+//     isFirstTimeRdc: isFirstTimeReducer,
+//     userInfoRdc: userInfoReducer
+//   }
+// );
