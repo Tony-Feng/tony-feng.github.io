c8f4bddb66c6c0f9f6c5715bf8303fd57a44edf4
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-11 05:45:34 -0400

    add json for proj

---
+++ src/assets/images/index.jsx
@@ -0,0 +1,9 @@
+import UserAvatar from './flask.jpg';
+import ProjImgOne from './1.png';
+import ProjImgTwo from './2.png';
+
+export default {
+  userAvatar: UserAvatar,
+  1: ProjImgOne,
+  2: ProjImgTwo
+};
