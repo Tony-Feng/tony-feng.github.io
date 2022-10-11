678f935f7a6c8a835dc4fc853e5d6e4ea4be95a0
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-11 07:52:46 -0400

    exclude user info reducer in persistence

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
-import storageSession from 'redux-persist/lib/storage/session';
 import { persistReducer } from 'redux-persist';
 import isDarkReducer from './is-dark-slice';
 import isFirstTimeReducer from './is-first-time-slice';
@@ -9,17 +8,12 @@ import userInfoReducer from './user-info-slice';
 const rootPersistConfig = {
   key: "root",
   storage: storage,
-  blacklist: ["userInfo"]
-};
-
-const userInfoPersistConfig = {
-  key: "userInfo",
-  storage: storageSession
+  blacklist: ["userInfoRdc"]
 };
 
 const rootReducer = combineReducers(
   {
-    userInfoRdc: persistReducer(userInfoPersistConfig, userInfoReducer),
+    userInfoRdc: userInfoReducer,
     isDarkRdc: isDarkReducer,
     isFirstTimeRdc: isFirstTimeReducer
   }
