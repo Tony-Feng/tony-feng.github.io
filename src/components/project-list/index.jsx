1431175f4a1d2b1bcb2c885147aecb517b8555ae
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-11 03:27:33 -0400

    tmp solution

--- src/components/project-list/index.jsx
+++ src/components/project-list/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { Grid, Card, CardActionArea, CardMedia, CardHeader, CardContent, Tooltip } from '@mui/material';
+import { Grid, Card, CardActionArea, CardMedia, Tooltip } from '@mui/material';
 import PropTypes from 'prop-types';
 import LinesEllipsis from 'react-lines-ellipsis';
 import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC';
@@ -11,6 +11,10 @@ import ProjectInfo from '../../assets/projects.json'; // todo: remove this
 import ImgOne from '../../assets/images/1.png'; // todo: remove this
 
 const ProjectList = (props) => {
+
+  const { numToLoad } = props;
+  const projects = ProjectInfo;
+  const isRetrieved = true;
 
   const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis);
 
@@ -31,17 +35,12 @@ const ProjectList = (props) => {
                   item["cover"] ? ( // the cover here can be ""; this field can be ignored as well, i.e., the field can be excluded from json
                     <>
                       <Grid item xs={ 12 } sm={ 12 } md={ 6 }>
-                        {/*<CardMedia component="img" image={ `${process.env.PUBLIC_URL}/${item["cover"]}` } alt={ `img_${item["id"]}_${item["title"]}` } /> /!* todo: remove `process.env.PUBLIC_URL` *!/*/}
                         <CardMedia component="img" image={ ImgOne } alt={ `img_${item["id"]}_${item["title"]}` } /> {/* todo: remove `process.env.PUBLIC_URL` */}
                       </Grid>
 
                       <Grid item xs={ 12 } sm={ 12 } md={ 6 }>
-                        {/*<StyledCardHeader title={ <ResponsiveEllipsis id={ item["id"] } text={ item["title"] } maxLine="1" ellipsis="..." basedOn="words" /> } titleTypographyProps={{ gutterBottom: false, variant: "h4", component: "div", align: "center", noWrap: true }} sx={{ py: "0px" }} /> /!* todo: display creation date of project *!/*/}
                         <StyledCardHeader title={ item["title"] } titleTypographyProps={{ gutterBottom: false, variant: "h4", component: "div", align: "center", noWrap: true }} sx={{ py: "5px" }} /> {/* todo: display creation date of project */}
 
-                        {/*<CardContent sx={{ py: "0px" }}>*/}
-                        {/*  <ResponsiveEllipsis id={ item["id"] } text={ item["brief"] } maxLine="5" ellipsis="..." basedOn="words" />*/}
-                        {/*</CardContent>*/}
                         <StyledCardContent>
                           <ResponsiveEllipsis id={ item["id"] } text={ item["brief"] } maxLine="5" ellipsis="..." basedOn="words" />
                         </StyledCardContent>
@@ -54,9 +53,6 @@ const ProjectList = (props) => {
                       </Grid>
 
                       <Grid item xs={ 12 } sm={ 12 } md={ 6 }>
-                        {/*<CardContent sx={{ py: "0px" }}>*/}
-                        {/*  <ResponsiveEllipsis id={ item["id"] } text={ item["brief"] } maxLine="5" ellipsis="..." basedOn="words" />*/}
-                        {/*</CardContent>*/}
                         <StyledCardContent>
                           <ResponsiveEllipsis id={ item["id"] } text={ item["brief"] } maxLine="5" ellipsis="..." basedOn="words" />
                         </StyledCardContent>
@@ -73,66 +69,23 @@ const ProjectList = (props) => {
     );
   };
 
-  // useEffect(() => {
-  //   const { getProjectAsync } = this.props;
-  //   }, []
-  // );
-  //
-  // componentDidMount() {
-  //   const { getProjectAsync } = this.props;
-  //
-  //   getProjectAsync(`${process.env.PUBLIC_URL}/projects.json`);
-  // };
-
-  const { numToLoad } = props;
-  const { projects } = ProjectInfo;
-  const isRetrieved = true;
-  const key = "9";
-  const value = {
-    "title": "999999999999999999999999999999999",
-    "brief": "Pellentesque pharetra eros pulvinar, aliquam ipsum a, rhoncus velit. Sed dictum lectus varius pharetra convallis. Quisque tempor sagittis lacus, ut gravida odio scelerisque in. Aenean imperdiet nisi at nunc convallis, ut tincidunt nisl fermentum. Nam nisi nulla, gravida ut scelerisque non, interdum a magna. Sed non dui ut nibh semper porta a quis nunc. Duis malesuada fermentum tortor nec pretium. Praesent quis risus est. Etiam ut euismod lectus. Phasellus pellentesque dignissim libero vel gravida. Cras vitae pharetra leo. In vestibulum, nulla quis mattis blandit, sem lectus hendrerit justo, et molestie sem magna eu leo.",
-    "tags": [
-      "t1",
-      "t2",
-      "t3",
-      "t4",
-      "t5",
-      "t6",
-      "t7"
-    ],
-    "cover": "images/9.png",
-    "id": 9
-  }
-
   return (
     <div>
-      <Grid container spacing={ 0 } direction="row" justifyContent="center" alignItems="center">
+      <Grid container spacing={ 0 } direction="row" justifyContent="center" alignItems="center" sx={{ width: { xs: "50px", sm: "1200px", md: "1800px" } }}>
           {
             (! isRetrieved) ? (
               <LoadingSpinner />
             ) : (
               <Grid item xs={ 10 } container spacing={ 4 } direction="row" justifyContent="center" alignItems="center">
-                {/*{*/}
-                {/*  projects && Object.entries(projects).map(([key, value]) =>*/}
-                {/*    numToLoad >= toInt(key, 10) && (*/}
-                {/*      <Grid item xs={ 12 } sm={ 12 } md={ 6 } key={ key }>*/}
-                {/*        { getCard(value) }*/}
-                {/*      </Grid>*/}
-                {/*    )*/}
-                {/*  )*/}
-                {/*}*/}
-                <Grid item xs={ 12 } sm={ 12 } md={ 6 } key={ key }>
-                  { getCard(value) }
-                </Grid>
-                <Grid item xs={ 12 } sm={ 12 } md={ 6 } key={ key }>
-                  { getCard(value) }
-                </Grid>
-                <Grid item xs={ 12 } sm={ 12 } md={ 6 } key={ key }>
-                  { getCard(value) }
-                </Grid>
-                <Grid item xs={ 12 } sm={ 12 } md={ 6 } key={ key }>
-                  { getCard(value) }
-                </Grid>
+                {
+                  projects && Object.entries(projects).map(([key, value]) =>
+                    numToLoad >= toInt(key, 10) && (
+                      <Grid item xs={ 12 } sm={ 12 } md={ 6 } key={ key }>
+                        { getCard(value) }
+                      </Grid>
+                    )
+                  )
+                }
               </Grid>
             )
           }
