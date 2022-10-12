8ab1d6de01b5ce0bbdab2ec481979b9ec595b66d
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-11 23:48:43 -0400

    clean up

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { AppBar, Grid, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
-import { computeWidth } from '../../utils/shared-functions';
 
 const Footer = (props) => {
 
   const [currentYear, ] = useState(new Date().getFullYear());
-  const [groupWidth, setGroupWidth] = useState(computeWidth(0.05));
   const isDark = useSelector(state => state.isDarkRdc.isDark);
 
   const appBarRef = useRef();
@@ -17,20 +15,12 @@ const Footer = (props) => {
     }, [handleFooterHeight, appBarRef]
   );
 
-  const handleWindowChange = () => {
-    setGroupWidth(computeWidth(0.05));
-  };
-
   useEffect(() => {
       getAppBarHeight();
       window.addEventListener("resize", getAppBarHeight);
 
-      handleWindowChange();
-      window.addEventListener("resize", handleWindowChange);
-
       return () => {
         window.removeEventListener("resize", getAppBarHeight);
-        window.removeEventListener("resize", handleWindowChange);
       };
     }, [getAppBarHeight]
   );
@@ -40,7 +30,6 @@ const Footer = (props) => {
       <AppBar position="static" sx={{ bgcolor: (isDark ? "#121212" : "#9CF"), boxShadow: 0, py: 2 }} ref={ appBarRef }>
         <Grid container spacing={ 0.1 } direction="column" justifyContent="space-evenly" alignItems="center" wrap="nowrap">
           <Grid item zeroMinWidth xs={ 12 }>
-            {/*<Typography noWrap variant="body1" component="div" align="center" sx={{ width: `${groupWidth}px`, maxWidth: `${groupWidth}px`, textOverflow: "ellipsis", overflow: "hidden" }}>{ "\u00A9" } TF 2021 - { currentYear } { "\u2022" } Developed using React and Redux</Typography> /!* todo: adjust the copyright content *!/*/}
             <Typography noWrap variant="body1" component="div" align="center" sx={{ width: "90vw", textOverflow: "ellipsis", overflow: "hidden" }}>{ "\u00A9" } TF 2021 - { currentYear } { "\u2022" } Developed using React and Redux</Typography> {/* todo: adjust the copyright content */}
           </Grid>
         </Grid>
