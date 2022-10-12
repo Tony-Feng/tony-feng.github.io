5b120a462caa97b72e71da883557448c00a139e3
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-11 23:39:48 -0400

    be more responsive

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -12,7 +12,13 @@ import images from '../../assets/images';
 
 const Home = () => { // todo: maybe use timeline to display education or experience
 
-  const [groupWidth, setGroupWidth] = useState(computeWidth(0.05));
+  const [groupWidth, setGroupWidth] = useState(
+    {
+      page: computeWidth(0.05),
+      avatar: computeWidth(0.43),
+      text: computeWidth(0.37)
+    }
+  );
 
   const userInfo = useSelector(state => state.userInfoRdc.userInfo);
   const { name, email, linkedin, github, bio, tags } = userInfo;
@@ -20,7 +26,13 @@ const Home = () => { // todo: maybe use timeline to display education or experie
   const { userAvatar } = images;
 
   const handleWindowChange = () => {
-    setGroupWidth(computeWidth(0.05));
+    setGroupWidth(
+      {
+        page: computeWidth(0.05),
+        avatar: computeWidth(0.43),
+        text: computeWidth(0.37)
+      }
+    );
   };
 
   useEffect(() => {
@@ -36,30 +48,32 @@ const Home = () => { // todo: maybe use timeline to display education or experie
   return (
     <Page>
 
-      <Grid container spacing={ 0 } direction="column" justifyContent="center" alignItems="center" sx={{ width: `${groupWidth}px`, maxWidth: `${groupWidth}px` }}>
+      <Grid container spacing={ 0 } direction="column" justifyContent="center" alignItems="center" sx={{ width: `${groupWidth["page"]}px`, maxWidth: `${groupWidth["page"]}px` }}>
 
         <Grid item xs={ 12 } sx={{ mt: 5 }}>
-          <Avatar alt={ name } src="" sx={{ bgcolor: "#9CF", objectFit: "contain" }}>{ name }</Avatar>
+          {/*<Avatar alt={ name } src={ userAvatar } align="center" sx={{ bgcolor: "#9CF", width: `${groupWidth["avatar"]}px`, height: `${groupWidth["avatar"]}px`, fontSize: "4vw", objectFit: "contain" }}>{ name }</Avatar>*/}
+          <Avatar alt={ name } src={ userAvatar } align="center" sx={{ bgcolor: "#9CF", width: "15vw", height: "15vw", fontSize: "4vw", objectFit: "contain" }}>{ name }</Avatar>
         </Grid>
 
         <Grid item xs={ 12 } sx={{ mt: 3 }}>
-          <Typography variant="h2" component="div">{ name }</Typography>
+          {/*<Typography variant="h2" component="div" align="center" sx={{ width: "40vw" }}>{ name }</Typography>*/}
+          <Typography variant="h2" component="div" align="center">{ name }</Typography>
         </Grid>
 
         <Grid item xs={ 12 } sx={{ mb: 2 }}>
-          <Stack spacing={ 3 } direction="row" justifyContent="center" alignItems="center">
+          <Stack spacing={{ xs: 1, sm: 3, md: 3 }} direction={{ xs: "column", sm: "row", md: "row" }} justifyContent="center" alignItems="center" align="center">
             <Tooltip title={ <><TooltipTag>Email</TooltipTag><TooltipTag>{ email }</TooltipTag></> } arrow leaveDelay={ 500 }>
-              <IconButton edge="start" color="inherit" aria-label="Email" variant="div" target="_blank" rel="noopener noreferrer" href={`mailto:${email}`}>
+              <IconButton edge="start" color="inherit" aria-label="Email" variant="div" target="_blank" rel="noopener noreferrer" href={`mailto:${email}`} size="small">
                 <Email sx={{ fontSize: 40 }} />
               </IconButton>
             </Tooltip>
             <Tooltip title="LinkedIn" arrow>
-              <IconButton edge="start" color="inherit" aria-label="LinkedIn" variant="div" target="_blank" rel="noopener noreferrer" href={ linkedin }>
+              <IconButton edge="start" color="inherit" aria-label="LinkedIn" variant="div" target="_blank" rel="noopener noreferrer" href={ linkedin } size="small">
                 <LinkedIn sx={{ fontSize: 40 }} />
               </IconButton>
             </Tooltip>
             <Tooltip title="GitHub" arrow>
-              <IconButton edge="start" color="inherit" aria-label="GitHub" variant="div" target="_blank" rel="noopener noreferrer" href={ github }>
+              <IconButton edge="start" color="inherit" aria-label="GitHub" variant="div" target="_blank" rel="noopener noreferrer" href={ github } size="small">
                 <GitHub sx={{ fontSize: 40 }} />
               </IconButton>
             </Tooltip>
@@ -67,18 +81,22 @@ const Home = () => { // todo: maybe use timeline to display education or experie
         </Grid>
 
         <Grid item xs={ 12 } sx={{ mx: 2 }}>
-          <Typography variant="body1" component="div" gutterBottom align="justify" sx={{ width: "100%", whiteSpace: "pre-line", objectFit: "contain" }}>{ bio }</Typography>
+          {/*<Typography variant="body1" component="div" gutterBottom align="justify" sx={{ width: `${groupWidth["text"]}px`, whiteSpace: "pre-line", objectFit: "contain" }}>{ bio }</Typography>*/}
+          {/*<Typography variant="body1" component="div" gutterBottom align="justify" sx={{ width: "32vw", whiteSpace: "pre-line", objectFit: "contain" }}>{ bio }</Typography>*/}
+          <Typography variant="h6" component="div" gutterBottom align="justify" sx={{ width: "42vw", whiteSpace: "pre-line", objectFit: "contain" }}>{ bio }</Typography>
         </Grid>
 
         <Grid item xs={ 12 } sx={{ mx: 2, my: 3 }}>
-          <Paper component="ul" elevation={ 8 } sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap", listStyle: "none", p: 2, m: 0, width: "100%", objectFit: "contain" }}>
+          {/*<Paper component="ul" elevation={ 8 } sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap", listStyle: "none", p: 2, m: 0, width: `${groupWidth["text"]}px`, objectFit: "contain" }}>*/}
+          <Paper component="ul" elevation={ 8 } sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap", listStyle: "none", p: 2, m: 0, width: "42vw", objectFit: "contain" }}>
             <Stack spacing={ 1 } direction="column" justifyContent="center" alignItems="center">
               <Typography variant="h4" component="div" gutterBottom align="center">Skills</Typography>
               <Box component="div" sx={{ textAlign: "center" }}>
                 {
                   tags.map((tag, idx) => {
                       return (
-                        <Chip label={tag} key={idx} component="li" sx={{ m: 1 }} />
+                        // <Chip label={tag} key={idx} component="li" sx={{ m: 1 }} />
+                        <Chip label={ <Typography style={{ whiteSpace: "normal" }}>{ tag }</Typography> } key={idx} component="li" sx={{ m: 1 }} />
                       );
                     }
                   )
@@ -90,9 +108,9 @@ const Home = () => { // todo: maybe use timeline to display education or experie
 
         <Grid item xs={ 12 } sx={{ mt: 6, mb: 8 }}> {/* todo: load more projects in real-time on home page while scrolling */}
           <Typography variant="h3" component="div" id="project" gutterBottom align="center" sx={{ mb: 6 }}>Selected Projects</Typography> {/* todo: display project in time order */}
-            <Suspense fallback={ <Loading /> }>
-              <ProjectList numToLoad={ 3 } />
-            </Suspense>
+          <Suspense fallback={ <Loading /> }>
+            <ProjectList numToLoad={ 3 } />
+          </Suspense>
           <Stack spacing={ 0 } direction="column" justifyContent="center" alignItems="center" sx={{ mx: 2 }}>
             <Button variant="outlined" size="medium" endIcon={ <KeyboardArrowRight /> } component={ RouterLink } to={ "/projects" } disabled sx={{ mt: 6, objectFit: "contain" }}>
               Stay Tuned for More Projects
