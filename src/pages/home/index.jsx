88012e6d778d306548564313c33942768ab6f36a
Tony Feng <43994607+Tony-Feng@users.noreply.github.com>
2022-10-12 14:45:29 -0400

    numToLoad is more visible

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -14,6 +14,8 @@ const Home = () => { // todo: maybe use timeline to display education or experie
   const userInfo = useSelector(state => state.userInfoRdc.userInfo);
   const { name, email, linkedin, github, bio, tags } = userInfo;
 
+  const numToLoad = 3; // todo: change to a dynamic value
+
   const { userAvatar } = images;
 
   return (
@@ -74,7 +76,7 @@ const Home = () => { // todo: maybe use timeline to display education or experie
         <Grid item xs={ 12 } sx={{ mt: 6, mb: 8 }}> {/* todo: load more projects in real-time on home page while scrolling */}
           <Typography variant="h3" component="div" id="project" gutterBottom align="center" sx={{ mb: 6 }}>Selected Projects</Typography> {/* todo: display project in time order */}
           <Suspense fallback={ <Loading /> }>
-            <ProjectList numToLoad={ 3 } />
+            <ProjectList numToLoad={ numToLoad } />
           </Suspense>
           <Stack spacing={ 0 } direction="column" justifyContent="center" alignItems="center" sx={{ mx: 2 }}>
             <Button variant="outlined" size="medium" endIcon={ <KeyboardArrowRight /> } component={ RouterLink } to={ "/projects" } disabled sx={{ mt: 6, objectFit: "contain" }}>
